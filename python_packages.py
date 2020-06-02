c1ea5420392dc0e8d89cb9c7b4794f38761f6c8d
khanmaster <eashkhan@hotmail.com>
2020-06-02 17:38:12 +0100

    python requests package example with bbc

--- python_packages.py
+++ python_packages.py
@@ -1,6 +1,6 @@
-install python package requests
-on command line run - pip install requests
-verify it with - pip -V
+# install python package requests
+# on command line run - pip install requests
+# verify it with - pip -V
 
 import requests
 
@@ -11,7 +11,7 @@ requests_bbc = requests.get("http://www.bbc.co.uk/")
 # print(requests_bbc.headers)
 # print(requests_bbc.content)
 
-second iteration to simplify our code and make it reusable
+# second iteration to simplify our code and make it reusable
 
 def check_response_code():
     if requests_bbc.status_code == 200:
@@ -34,7 +34,7 @@ def check_response_code():
 
 print(check_response_code())
 
-requests goes one step further in simplifying this process for us.
-If you use a request_bcc instance in a conditional expression,
-it will evaluate to True if the status code was between 200 and 400, and False otherwise.
-Therefore, you can simplify the last example by rewriting the if statement as above:
+# requests goes one step further in simplifying this process for us.
+# If you use a request_bcc instance in a conditional expression,
+# it will evaluate to True if the status code was between 200 and 400, and False otherwise.
+# Therefore, you can simplify the last example by rewriting the if statement as above:

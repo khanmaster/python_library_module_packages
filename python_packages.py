e4ea06d0fffdc6fda96d9cdf8ce74b7c794ead87
khanmaster <eashkhan@hotmail.com>
2020-06-03 10:38:10 +0100

    request package examples with 3 iterations

--- python_packages.py
+++ python_packages.py
@@ -4,37 +4,44 @@
 
 import requests
 
-requests_bbc = requests.get("http://www.bbc.co.uk/")
+response_bbc = requests.get("http://www.bbc.co.uk/")
 
+print(response_bbc.status_code)
+print(response_bbc.headers)
+# print(response_bbc.content)
 
-# print(requests_bbc.status_code)
-# print(requests_bbc.headers)
-# print(requests_bbc.content)
+# first iteration
+if response_bbc.status_code == 200:
+    print("Response successful")
+elif response_bbc.status_code == 404:
+    print(" Sorry page not found")
+else:
+    print(" Opps... something went wrong")
 
 # second iteration to simplify our code and make it reusable
-
-def check_response_code():
-    if requests_bbc.status_code == 200:
-        print("Response successful")
-    elif requests_bbc.status_code == 404:
-        print(" Sorry page not found")
-    else:
-        print(" Opps some went wrong")
-print(check_response_code())
-
-# 3rd iteration to apply best practice
-def check_response_code():
-
-    if requests_bbc:
-        print("Response successful")
-    elif requests_bbc:
-        print(" Sorry page not found")
-    else:
-        print(" Opps some went wrong")
-
-print(check_response_code())
+#
+# def check_response_code():
+#     if response_bbc.status_code == 200:
+#         print("Response successful")
+#     elif response_bbc.status_code == 404:
+#         print(" Sorry page not found")
+#     else:
+#         print(" Opps... something went wrong")
+# print(check_response_code())
+#
+# # 3rd iteration to apply best practice
+# # def check_response_code():
+#
+# #     if response_bbc:
+# #         print("Response successful")
+#     elif response_bbc:
+#         print(" Sorry page not found")
+#     else:
+#         print(" Opps some went wrong")
+#
+# print(check_response_code())
 
 # requests goes one step further in simplifying this process for us.
-# If you use a request_bcc instance in a conditional expression,
-# it will evaluate to True if the status code was between 200 and 400, and False otherwise.
+# If you use a response_bcc instance in a conditional expression,
+# It will evaluate to True if the status code was between 200 and 400, and False otherwise.
 # Therefore, you can simplify the last example by rewriting the if statement as above:
